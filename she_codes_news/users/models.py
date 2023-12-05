95ae21647673dc87531fe22f70017808c4bdc6e4
COHALLY <CATHY.O'HALLORAN@RWWA.COM.AU>
2023-12-06 07:47:35 +0800

    Profile creation.

--- she_codes_news/users/models.py
+++ she_codes_news/users/models.py
@@ -1,7 +1,14 @@
+from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
 class CustomUser(AbstractUser):
+    profile_image = models.URLField(blank=True)
+    bio = models.TextField(blank=True)
     pass
 
     def _str_(self):
         return self.username
+    
+
+
